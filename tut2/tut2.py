85bd00af8cc45025f3653c55f09e257ab6468dbe
Tyrone van Balla <tvanballa@ska.ac.za>
2015-10-27 15:12:42 +0200

    Removed all dependency on old corr library

--- tut2/tut2.py
+++ tut2/tut2.py
@@ -7,7 +7,7 @@ Author: Jason Manley, August 2009.
 Updated by: Tyrone van Balla, July 2015
 Updated for CASPER 2016 workshop. Updated to use casperfpga library and for ROACH-2
 '''
-import corr, casperfpga, time, struct, sys, logging, socket
+import casperfpga, time, struct, sys, logging, socket
 
 #Decide where we're going to send the data, and from which addresses:
 # should match addresses specified in gbe blocks in simulink model
@@ -39,6 +39,45 @@ def exit_clean():
     except: pass
     exit()
 
+# debug log handler
+class DebugLogHandler(logging.Handler):
+    """A logger for KATCP tests."""
+
+    def __init__(self,max_len=100):
+        """Create a TestLogHandler.
+            @param max_len Integer: The maximum number of log entries
+                                    to store. After this, will wrap.
+        """
+        logging.Handler.__init__(self)
+        self._max_len = max_len
+        self._records = []
+
+    def emit(self, record):
+        """Handle the arrival of a log message."""
+        if len(self._records) >= self._max_len: self._records.pop(0)
+        self._records.append(record)
+
+    def clear(self):
+        """Clear the list of remembered logs."""
+        self._records = []
+
+    def setMaxLen(self,max_len):
+        self._max_len=max_len
+
+    def printMessages(self):
+        for i in self._records:
+            if i.exc_info:
+                print '%s: %s Exception: '%(i.name,i.msg),i.exc_info[0:-1]
+            else:    
+                if i.levelno < logging.WARNING: 
+                    print '%s: %s'%(i.name,i.msg)
+                elif (i.levelno >= logging.WARNING) and (i.levelno < logging.ERROR):
+                    print '%s: %s'%(i.name,i.msg)
+                elif i.levelno >= logging.ERROR: 
+                    print '%s: %s'%(i.name,i.msg)
+                else:
+                    print '%s: %s'%(i.name,i.msg)
+
 if __name__ == '__main__':
     from optparse import OptionParser
 
@@ -65,7 +104,7 @@ if __name__ == '__main__':
     if opts.fpg != '':
         fpgfile = opts.fpg
 try:
-    lh = corr.log_handlers.DebugLogHandler()
+    lh = DebugLogHandler()
     logger = logging.getLogger(roach)
     logger.addHandler(lh)
     logger.setLevel(10)
