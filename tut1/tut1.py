c4f650c773490686586a49f1921a2659909fb74e
Tyrone van Balla <tvanballa@ska.ac.za>
2016-01-06 09:54:06 +0200

    cleaned up tut1 files

--- tut1/tut1.py
+++ tut1/tut1.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python
+<<<<<<< HEAD
+
+=======
+>>>>>>> 227b7d107a8ce05d7c10f6f3b5a8638ed5a8899a
 '''
 Python script for Casper Tutorial 1
 Author: Tyrone van Balla (SKA Africa), July 2015
 Updated for 2016 CASPER Workshop. Updated Tutorial 1 for use on ROACH2 and to use casperfpga library
 '''
 
-import casperfpga, time, struct, sys, logging, socket
+import casperfpga
+import time
+import sys
 
 # set default fpg file
-fpgfile = 'tut1_updated_2015.fpg'
+fpgfile = 'tut1.fpg'
 
 def exit_fail():
-	print 'FAILURE DETECTED. Log entries:\n'
+	print 'FAILURE DETECTED.\n'
 	exit()
 
 def exit_clean():
@@ -22,22 +28,23 @@ def exit_clean():
 
 # get command line arguments
 if __name__ == '__main__':
-    from optparse import OptionParser
+    import argparse
 
-    p = OptionParser()
-    p.set_usage('tut1_updated.py <ROACH_HOSTNAME_or_IP> [options]')
-    p.set_description(__doc__)
-    p.add_option('-f', '--fpgfile', dest='fpg', type='str', default=fpgfile,
-        help='Specify the fpg file to load')  
-    opts, args = p.parse_args(sys.argv[1:])
+    parser = argparse.ArgumentParser()
+    parser.add_argument("roach", help="<ROACH_HOSTNAME_or_IP>")
+    parser.add_argument("-f", "--fpgfile", help="Specify bitstream to load", type=str, default=fpgfile)
+    parser.add_argument("-n", "--noadd", help="disable adder testing", action="store_true")    
+    
+    args = parser.parse_args()
 
-    if args==[]:
+    if args.roach=='':
         print 'Please specify a ROACH board. \nExiting.'
         exit()
     else:
-        roach2 = args[0]
-    if opts.fpg != '':
-        fpgfile = opts.fpg
+        roach2 = args.roach
+    
+    if args.fpgfile != '':
+        fpgfile = args.fpgfile
 
 # instantiate fpga object
 print('Connecting to server %s... '%(roach2)),
@@ -58,30 +65,31 @@ fpga.upload_to_ram_and_program(fpgfile)
 time.sleep(10)
 print 'ok'
 
-# get variables to assign to registers to be used in adder
-print '---------------------------' 
-print 'Enter values for adder: \n'
-var_a = int(raw_input("Enter a value for a: "))
-var_b = int(raw_input("Enter a value for b: "))
-print '\n'    
-print 'Assigning values to registers. . . .',
-try:
-	fpga.write_int('a', var_a)
-	fpga.write_int('b', var_b)
-except:
-	print "ERROR writing values to registers"
-	exit_fail()
+if (not args.noadd):
+    # get variables to assign to registers to be used in adder
+    print '---------------------------' 
+    print 'Enter values for adder: \n'
+    var_a = int(raw_input("Enter a value for a: "))
+    var_b = int(raw_input("Enter a value for b: "))
+    print '\n'    
+    print 'Assigning values to registers. . . .',
+    try:
+            fpga.write_int('a', var_a)
+            fpga.write_int('b', var_b)
+    except:
+            print "ERROR writing values to registers"
+            exit_fail()
 
-print 'ok\n'
-print '---------------------------'
-print 'Reading value from sum_a_b: output from adder: ',
-try:
-	print fpga.read_int('sum_a_b')
-except:
-	print "ERROR reading from register"
-	exit_fail()
-print 'Read adder output ok'
-print '---------------------------\n'
+    print 'ok\n'
+    print '---------------------------'
+    print 'Reading value from sum_a_b: output from adder: ',
+    try:
+            print fpga.read_int('sum_a_b')
+    except:
+            print "ERROR reading from register"
+            exit_fail()
+    print 'Read adder output ok'
+    print '---------------------------\n'
 
 counter_en = raw_input("Enable counter? (y/n): ")
 if counter_en == 'y':
